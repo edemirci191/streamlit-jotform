92776be99f0265b038b28189e37ec27f97d9edf4
Emre Demirci <edemirci@sabanciuniv.edu>
2021-08-31 12:20:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -314,6 +314,8 @@ def main():
       show_df.to_html(escape=False)
       show_df['Similar Questions'] = items
       show_df['Thread URL'] = lst
+      dummyvar = topic_recommend("How to create a form",dictionary)
+      st.subheader("Most Related Topic is")
       st.subheader('Recommendations')
       st.table(show_df)
 
