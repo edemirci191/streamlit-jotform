7592370fd26b11cc27ec14d3c61b2cd4a8626936
Emre Demirci <edemirci@sabanciuniv.edu>
2021-09-01 12:33:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,10 +96,11 @@ def topic_recommend(user_input):
        dictionary = load_dictionary()
        bow_vector = dictionary.doc2bow(preprocess(user_input))
        for index,score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
-              if(score > 0.2):
-                     for i in range(4):
-                            if(lda_model.show_topic(index, 4)[i][0] not in topic_result):
-                                   topic_result.append(lda_model.show_topic(index, 4)[i][0])
+              if(score > 0.5):
+                     for i in range(10):
+                            if(lda_model.show_topic(index, 10)[i][0] not in topic_result):
+                                   topic_result.append(lda_model.show_topic(index, 10)[i][0])
+              break
        return topic_result
 
 
