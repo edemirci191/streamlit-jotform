eb05ff7f8b7219d9fb472329f4657e0f5616d5e7
Emre Demirci <edemirci@sabanciuniv.edu>
2021-09-05 22:35:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,226 +84,229 @@ def main():
   st.sidebar.image("https://storage.googleapis.com/jotform-recommender.appspot.com/jotform-logo-transparent-800x400.png",width=300)
   menu = ["Similar Questions","Relevant Topics"]
   choice = st.sidebar.selectbox("Menu",menu)
-  st.title("Jotform Support Forum Question Recommender")
-  st.subheader("Overview")
-  st.write("Purpose of this application is to recommend the user similar questions that has been asked before by other users. When the user asks a new question other already answered similar questions are going to be recommended to the user in English and also in his/her native language.")
-  uinput = st.text_input("Question","How can I create a succesful survey form ?")    
-  user_input = st.text_area("Description","I want to learn how to create succesful survey form")
-  st.button("Search Question")
-  allinput = uinput +" "+ user_input
-  user_input = allinput
-  st.image('https://storage.googleapis.com/jotform-recommender.appspot.com/podo_7.png',width=264)
-  not_found = 1
-  # in else part embedfn=embed_.. changed to embedfn = embed
-  if any(word in user_input for word in converted_list):
-    print('Your sentence contains profanity words, please try again')
-  else:
-    l_index = None
-    map = None
-    embedfn = None
-    rpm = None
-    lg = detect(user_input)
-    varforid = None
-    
-    if lg == 'en':
-      if not os.path.exists('en_from_url'):
-        url_en = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_en"
-        r_en = requests.get(url_en, stream = True)
-        with open("en_from_url","wb") as f:
-          for block in r_en.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_en = "en_from_url"
-      index_en = annoy.AnnoyIndex(embedding_dimension)
-      index_en.load(index_filename_en, prefault=True)
-      l_index = index_en
-      map = mapping_en
-      embedfn = embed
-      rpm = random_projection_matrix_en
-      not_found = 0
-      varforid = question_en
-      
-    if lg == 'es':
-      if not os.path.exists('es_from_url'):
-        url_es = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_es"
-        r_es = requests.get(url_es, stream = True)
-        with open("es_from_url","wb") as f:
-          for block in r_es.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_es = "es_from_url"
-      index_es = annoy.AnnoyIndex(embedding_dimension)
-      index_es.load(index_filename_es, prefault=True)
-      l_index = index_es
-      map = mapping_es
-      embedfn = embed
-      rpm = random_projection_matrix_es
-      not_found = 0
-      varforid = question_es
-     
-    if lg == 'tr':
-      if not os.path.exists('tr_from_url'):
-        url_tr = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_tr"
-        r_tr = requests.get(url_tr, stream = True)
-        with open("tr_from_url","wb") as f:
-          for block in r_tr.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_tr = "tr_from_url"
-      index_tr = annoy.AnnoyIndex(embedding_dimension)
-      index_tr.load(index_filename_tr, prefault=True)
-      l_index = index_tr
-      map = mapping_tr
-      embedfn = embed
-      rpm = random_projection_matrix_tr
-      not_found = 0
-      varforid = question_tr
-      
-    if lg == 'fr':
-      if not os.path.exists('fr_from_url'):
-        url_fr = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_fr"
-        r_fr = requests.get(url_fr, stream = True)
-        with open("fr_from_url","wb") as f:
-          for block in r_fr.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_fr = "fr_from_url"
-      index_fr = annoy.AnnoyIndex(embedding_dimension)
-      index_fr.load(index_filename_fr, prefault=True)
-      l_index = index_fr
-      map = mapping_fr
-      embedfn = embed
-      rpm = random_projection_matrix_fr
-      not_found = 0
-      varforid=question_fr
-      
-    if lg == 'de':
-      if not os.path.exists('de_from_url'):
-        url_de = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_de"
-        r_de = requests.get(url_de, stream = True)
-        with open("de_from_url","wb") as f:
-          for block in r_de.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_de = "de_from_url"
-      index_de = annoy.AnnoyIndex(embedding_dimension)
-      index_de.load(index_filename_de, prefault=True)
-      l_index = index_de
-      map = mapping_de
-      embedfn = embed
-      rpm = random_projection_matrix_de
-      not_found = 0
-      varforid = question_de
-      
-    if lg == 'nl':
-      if not os.path.exists('nl_from_url'):
-        url_nl = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_nl"
-        r_nl = requests.get(url_nl, stream = True)
-        with open("nl_from_url","wb") as f:
-          for block in r_nl.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_nl = "nl_from_url"
-      index_nl = annoy.AnnoyIndex(embedding_dimension)
-      index_nl.load(index_filename_nl, prefault=True)
-      l_index = index_nl
-      map = mapping_nl
-      embedfn = embed
-      rpm = random_projection_matrix_nl
-      not_found = 0
-      varforid= question_nl
-
-    if lg == 'pt':
-      if not os.path.exists('pt_from_url'):
-        url_pt = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_pt"
-        r_pt = requests.get(url_pt, stream = True)
-        with open("pt_from_url","wb") as f:
-          for block in r_pt.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_pt = "pt_from_url"
-      index_pt = annoy.AnnoyIndex(embedding_dimension)
-      index_pt.load(index_filename_pt, prefault=True)
-      l_index = index_pt
-      map = mapping_pt
-      embedfn = embed
-      rpm = random_projection_matrix_pt
-      not_found = 0
-      varforid = question_pt
-
-    if lg == 'it':
-      if not os.path.exists('it_from_url'):
-        url_it = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_it"
-        r_it = requests.get(url_it, stream = True)
-        with open("it_from_url","wb") as f:
-          for block in r_it.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_it = "it_from_url"
-      index_it = annoy.AnnoyIndex(embedding_dimension)
-      index_it.load(index_filename_it, prefault=True)
-      l_index = index_it
-      map = mapping_it
-      embedfn = embed
-      rpm = random_projection_matrix_it
-      not_found = 0
-      varforid = question_it
-
-    if not_found == 1:
-      if not os.path.exists('en_from_url'):
-        url_en = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_en"
-        r_en = requests.get(url_en, stream = True)
-        with open("en_from_url","wb") as f:
-          for block in r_en.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-      index_filename_en = "en_from_url"
-      index_en = annoy.AnnoyIndex(embedding_dimension)
-      index_en.load(index_filename_en, prefault=True)
-      l_index = index_en
-      map = mapping_en
-      embedfn = embed
-      rpm = random_projection_matrix_en
-      lg = 'en'
-      varforid = question_en
-
-    query_embedding = extract_embeddings(user_input,embedfn,rpm)
-    items,ids = find_similar_items(l_index,map,query_embedding, 5)
-    lst=[]
-    show_df = pd.DataFrame()
-    if lg != 'en':
-      index_filename_en = "en_from_url"
-      index_en = annoy.AnnoyIndex(embedding_dimension)
-      index_en.load(index_filename_en, prefault=True)
-      query_embedding_en = extract_embeddings(user_input,embedfn,random_projection_matrix_en)
-      items_en,ids_en = find_similar_items(index_en,mapping_en,query_embedding_en, 5)
-      extended_items = items_en + items
-      for j in ids_en:
-        lst.append("https://www.jotform.com/answers/"+str(question_en.iloc[j]['id']))
-      for i in ids:
-        lst.append("https://www.jotform.com/answers/"+str(varforid.iloc[i]['id']))
-      show_df.to_html(escape=False)
-      show_df['Similar Questions'] = extended_items
-      show_df['Thread URL'] = lst
-      #st.subheader("Most Related Topics for")
-      #topic = topic_recommend(extended_items[0])
-      #st.write(topic)
-      st.subheader('Recommendations')
-      st.table(show_df)
-      #st.write("[https://www.jotform.com/answers/]" + str(lst[1])) hyperlink with constant id
+  if choice == "Similar Questions":
+    st.title("Jotform Support Forum Question Recommender")
+    st.subheader("Overview")
+    st.write("Purpose of this application is to recommend the user similar questions that has been asked before by other users. When the user asks a new question other already answered similar questions are going to be recommended to the user in English and also in his/her native language.")
+    uinput = st.text_input("Question","How can I create a succesful survey form ?")    
+    user_input = st.text_area("Description","I want to learn how to create succesful survey form")
+    st.button("Search Question")
+    allinput = uinput +" "+ user_input
+    user_input = allinput
+    st.image('https://storage.googleapis.com/jotform-recommender.appspot.com/podo_7.png',width=264)
+    not_found = 1
+    # in else part embedfn=embed_.. changed to embedfn = embed
+    if any(word in user_input for word in converted_list):
+      print('Your sentence contains profanity words, please try again')
     else:
-      for i in ids:
-        lst.append("https://www.jotform.com/answers/" + str(varforid.iloc[i]['id']))
+      l_index = None
+      map = None
+      embedfn = None
+      rpm = None
+      lg = detect(user_input)
+      varforid = None
 
-      show_df.to_html(escape=False)
-      show_df['Similar Questions'] = items
-      show_df['Thread URL'] = lst
-      #st.subheader("Most Related Topics for")
-      #st.write(user_input)
-      #topic = topic_recommend(user_input)
-      #st.write(topic)
-      st.subheader('Recommendations')
-      st.table(show_df)
+      if lg == 'en':
+        if not os.path.exists('en_from_url'):
+          url_en = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_en"
+          r_en = requests.get(url_en, stream = True)
+          with open("en_from_url","wb") as f:
+            for block in r_en.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_en = "en_from_url"
+        index_en = annoy.AnnoyIndex(embedding_dimension)
+        index_en.load(index_filename_en, prefault=True)
+        l_index = index_en
+        map = mapping_en
+        embedfn = embed
+        rpm = random_projection_matrix_en
+        not_found = 0
+        varforid = question_en
 
+      if lg == 'es':
+        if not os.path.exists('es_from_url'):
+          url_es = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_es"
+          r_es = requests.get(url_es, stream = True)
+          with open("es_from_url","wb") as f:
+            for block in r_es.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_es = "es_from_url"
+        index_es = annoy.AnnoyIndex(embedding_dimension)
+        index_es.load(index_filename_es, prefault=True)
+        l_index = index_es
+        map = mapping_es
+        embedfn = embed
+        rpm = random_projection_matrix_es
+        not_found = 0
+        varforid = question_es
+
+      if lg == 'tr':
+        if not os.path.exists('tr_from_url'):
+          url_tr = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_tr"
+          r_tr = requests.get(url_tr, stream = True)
+          with open("tr_from_url","wb") as f:
+            for block in r_tr.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_tr = "tr_from_url"
+        index_tr = annoy.AnnoyIndex(embedding_dimension)
+        index_tr.load(index_filename_tr, prefault=True)
+        l_index = index_tr
+        map = mapping_tr
+        embedfn = embed
+        rpm = random_projection_matrix_tr
+        not_found = 0
+        varforid = question_tr
+
+      if lg == 'fr':
+        if not os.path.exists('fr_from_url'):
+          url_fr = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_fr"
+          r_fr = requests.get(url_fr, stream = True)
+          with open("fr_from_url","wb") as f:
+            for block in r_fr.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_fr = "fr_from_url"
+        index_fr = annoy.AnnoyIndex(embedding_dimension)
+        index_fr.load(index_filename_fr, prefault=True)
+        l_index = index_fr
+        map = mapping_fr
+        embedfn = embed
+        rpm = random_projection_matrix_fr
+        not_found = 0
+        varforid=question_fr
+
+      if lg == 'de':
+        if not os.path.exists('de_from_url'):
+          url_de = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_de"
+          r_de = requests.get(url_de, stream = True)
+          with open("de_from_url","wb") as f:
+            for block in r_de.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_de = "de_from_url"
+        index_de = annoy.AnnoyIndex(embedding_dimension)
+        index_de.load(index_filename_de, prefault=True)
+        l_index = index_de
+        map = mapping_de
+        embedfn = embed
+        rpm = random_projection_matrix_de
+        not_found = 0
+        varforid = question_de
+
+      if lg == 'nl':
+        if not os.path.exists('nl_from_url'):
+          url_nl = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_nl"
+          r_nl = requests.get(url_nl, stream = True)
+          with open("nl_from_url","wb") as f:
+            for block in r_nl.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_nl = "nl_from_url"
+        index_nl = annoy.AnnoyIndex(embedding_dimension)
+        index_nl.load(index_filename_nl, prefault=True)
+        l_index = index_nl
+        map = mapping_nl
+        embedfn = embed
+        rpm = random_projection_matrix_nl
+        not_found = 0
+        varforid= question_nl
+
+      if lg == 'pt':
+        if not os.path.exists('pt_from_url'):
+          url_pt = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_pt"
+          r_pt = requests.get(url_pt, stream = True)
+          with open("pt_from_url","wb") as f:
+            for block in r_pt.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_pt = "pt_from_url"
+        index_pt = annoy.AnnoyIndex(embedding_dimension)
+        index_pt.load(index_filename_pt, prefault=True)
+        l_index = index_pt
+        map = mapping_pt
+        embedfn = embed
+        rpm = random_projection_matrix_pt
+        not_found = 0
+        varforid = question_pt
+
+      if lg == 'it':
+        if not os.path.exists('it_from_url'):
+          url_it = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_it"
+          r_it = requests.get(url_it, stream = True)
+          with open("it_from_url","wb") as f:
+            for block in r_it.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_it = "it_from_url"
+        index_it = annoy.AnnoyIndex(embedding_dimension)
+        index_it.load(index_filename_it, prefault=True)
+        l_index = index_it
+        map = mapping_it
+        embedfn = embed
+        rpm = random_projection_matrix_it
+        not_found = 0
+        varforid = question_it
+
+      if not_found == 1:
+        if not os.path.exists('en_from_url'):
+          url_en = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_en"
+          r_en = requests.get(url_en, stream = True)
+          with open("en_from_url","wb") as f:
+            for block in r_en.iter_content(chunk_size = 8192):
+              if block:
+                f.write(block)
+        index_filename_en = "en_from_url"
+        index_en = annoy.AnnoyIndex(embedding_dimension)
+        index_en.load(index_filename_en, prefault=True)
+        l_index = index_en
+        map = mapping_en
+        embedfn = embed
+        rpm = random_projection_matrix_en
+        lg = 'en'
+        varforid = question_en
+
+      query_embedding = extract_embeddings(user_input,embedfn,rpm)
+      items,ids = find_similar_items(l_index,map,query_embedding, 5)
+      lst=[]
+      show_df = pd.DataFrame()
+      if lg != 'en':
+        index_filename_en = "en_from_url"
+        index_en = annoy.AnnoyIndex(embedding_dimension)
+        index_en.load(index_filename_en, prefault=True)
+        query_embedding_en = extract_embeddings(user_input,embedfn,random_projection_matrix_en)
+        items_en,ids_en = find_similar_items(index_en,mapping_en,query_embedding_en, 5)
+        extended_items = items_en + items
+        for j in ids_en:
+          lst.append("https://www.jotform.com/answers/"+str(question_en.iloc[j]['id']))
+        for i in ids:
+          lst.append("https://www.jotform.com/answers/"+str(varforid.iloc[i]['id']))
+        show_df.to_html(escape=False)
+        show_df['Similar Questions'] = extended_items
+        show_df['Thread URL'] = lst
+        #st.subheader("Most Related Topics for")
+        #topic = topic_recommend(extended_items[0])
+        #st.write(topic)
+        st.subheader('Recommendations')
+        st.table(show_df)
+        #st.write("[https://www.jotform.com/answers/]" + str(lst[1])) hyperlink with constant id
+      else:
+        for i in ids:
+          lst.append("https://www.jotform.com/answers/" + str(varforid.iloc[i]['id']))
+
+        show_df.to_html(escape=False)
+        show_df['Similar Questions'] = items
+        show_df['Thread URL'] = lst
+        #st.subheader("Most Related Topics for")
+        #st.write(user_input)
+        #topic = topic_recommend(user_input)
+        #st.write(topic)
+        st.subheader('Recommendations')
+        st.table(show_df)
+      elif choice == "Relevant Topics":
+        st.write("Relevant Topics")
+       
 if __name__ == '__main__':
   main()
