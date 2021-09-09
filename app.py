e88b3d36e58f735cf86692fd8f087ab4e4164db6
Emre Demirci <edemirci@sabanciuniv.edu>
2021-09-09 12:09:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -308,13 +308,6 @@ def main():
       lst=[]
       show_df = pd.DataFrame()
       if lg != 'en':
-        url_en = "https://storage.googleapis.com/jotform-recommender.appspot.com/index_en"
-        r_en = requests.get(url_en, stream = True)
-        with open("en_from_url","wb") as f:
-          for block in r_en.iter_content(chunk_size = 8192):
-            if block:
-              f.write(block)
-        #added later------------------------------------
         index_filename_en = "en_from_url"
         index_en = annoy.AnnoyIndex(embedding_dimension)
         index_en.load(index_filename_en, prefault=True)
